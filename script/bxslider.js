597ade038f6d28b7c1c35a80be305541421bf60e
tanyapetrova26 <108935676+tanyapetrova26@users.noreply.github.com>
2022-09-12 16:37:40 +0300

    Add files via upload

---
+++ script/bxslider.js
@@ -0,0 +1,26 @@
+$('.carusel').bxSlider({
+    mode: 'horizontal',
+    pager: true,
+    controls: false,
+    InfiniteLoop: true,
+    touchEnabled: true,
+    hideControlOnEnd: false,
+    tickerHover: true,
+    adaptiveHeight: true,
+    infinite: true,
+});
+
+
+
+
+
+$('.clients_carusel').bxSlider({
+    mode: 'fade',
+    pager: true,
+    controls: false,
+    InfiniteLoop: true,
+    touchEnabled: true,
+    hideControlOnEnd: false,
+    tickerHover: true,
+    adaptiveHeight: true,
+})
